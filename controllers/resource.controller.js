4002dd6b01cc02ca4312a440b8b4f512e0bb5fde
MatSosa1 <dmsosa3@espe.edu.ec>
2026-06-17 22:38:06 -0500

    feat: simular caida en service-beta con captura de excepciones Sentry

--- controllers/resource.controller.js
+++ controllers/resource.controller.js
@@ -1,3 +1,5 @@
+import * as Sentry from '@sentry/node';
+
 export class ResourceController {
     /**
      * Simula un recurso privado del Microservicio Alpha.
@@ -16,9 +18,13 @@ export class ResourceController {
      * Simula un recurso privado del Microservicio Beta.
      */
     static getBetaPrivateData(req, res) {
-        return res.status(200).json({
-            message: "Acceso exitoso al Servicio Beta",
-            user: req.user
-        });
+        try {
+            throw new Error('Error en servicio Beta');
+        } catch (error) {
+            Sentry.captureException(error, {
+                tags: { source: 'socket.io', event: 'error' },
+                extra: { browser: 'Postman' }
+            })
+        }
     }
 }
