fcae3101ef1dfe052536028ed0073e9cf5deaea0
MatSosa1 <dmsosa3@espe.edu.ec>
2026-06-17 22:26:50 -0500

    feat: simular caida en service-alpha para verificar error tracking

--- controllers/resource.controller.js
+++ controllers/resource.controller.js
@@ -3,10 +3,13 @@ export class ResourceController {
      * Simula un recurso privado del Microservicio Alpha.
      */
     static getAlphaPrivateData(req, res) {
-        return res.status(200).json({
-            message: "Acceso exitoso al Servicio Alpha",
-            user: req.user
-        });
+        // return res.status(200).json({
+        //     message: "Acceso exitoso al Servicio Alpha",
+        //     user: req.user
+        // });
+
+        // Error para Sentry
+        throw new Error('Conexión perdida con DB');
     }
 
     /**
